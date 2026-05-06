9cf0bd6e4e82f464d4d61040eb861d497388ca85
protocol_1903 <67478786+protocol-1903@users.noreply.github.com>
2026-05-05 23:29:16 -0700

    Features:   - Added perel.get_fluidbox_neighoburs_by_fluidbox_and_connection   - Added perel.get_fluidbox_targets   - Added perel.get_fluidbox_targets_by_fluidbox_and_connection   - Added perel.get_possible_fluidbox_neighbours_by_fluidbox_and_connection Changes:   - Added optional `include_linked` flag to methods that support `include_undergrounds`   - Updated annotations to use dictionary references where possible

--- util/scripts/fluids.lua
+++ util/scripts/fluids.lua
@@ -3,12 +3,13 @@ require "__perel__.util.scripts.general"
 --- returns any fluidbox connected neighbours. by default only includes normal connections
 ---@param entity LuaEntity 
 ---@param include_undergrounds? boolean whether or not to include underground connections, default false
+---@param include_linked? boolean whether or not to include linked connections, default false
 ---@return LuaEntity[] neighbours
-perel.get_fluidbox_neighoburs = function(entity, include_undergrounds)
+perel.get_fluidbox_neighoburs = function(entity, include_undergrounds, include_linked)
   local neighbours = {}
   for i = 1, #entity.fluidbox do
     for _, pipe_connection in pairs(entity.fluidbox.get_pipe_connections(i)) do
-      if pipe_connection.target and (include_undergrounds and pipe_connection.connection_type ~= "linked" or pipe_connection.connection_type == "normal") then
+      if pipe_connection.target and (include_undergrounds and pipe_connection.connection_type ~= "linked" or include_linked and pipe_connection.connection_type ~= "underground" or pipe_connection.connection_type == "normal") then
         neighbours[#neighbours+1] = pipe_connection.target.owner
       end
     end
@@ -16,25 +17,79 @@ perel.get_fluidbox_neighoburs = function(entity, include_undergrounds)
   return neighbours
 end
 
+--- returns any fluidbox connected neighbours, indexed by fluidbox and connection id. by default only includes normal connections
+---@param entity LuaEntity 
+---@param include_undergrounds? boolean whether or not to include underground connections, default false
+---@param include_linked? boolean whether or not to include linked connections, default false
+---@return LuaEntity[][] neighbours
+perel.get_fluidbox_neighoburs_by_fluidbox_and_connection = function(entity, include_undergrounds, include_linked)
+  local neighbours = {}
+  for i = 1, #entity.fluidbox do
+    neighbours[i] = {}
+    for j, pipe_connection in pairs(entity.fluidbox.get_pipe_connections(i)) do
+      if pipe_connection.target and (include_undergrounds and pipe_connection.connection_type ~= "linked" or include_linked and pipe_connection.connection_type ~= "underground" or pipe_connection.connection_type == "normal") then
+        neighbours[i][j] = pipe_connection.target.owner
+      end
+    end
+  end
+  return neighbours
+end
+
+--- returns any fluidbox targets. by default only includes normal connections
+---@param entity LuaEntity 
+---@param include_undergrounds? boolean whether or not to include underground connections, default false
+---@param include_linked? boolean whether or not to include linked connections, default false
+---@return {target: LuaFluidBox, target_fluidbox_index: int, target_pipe_connection_index: int}[] fluidbox_targets
+perel.get_fluidbox_targets = function(entity, include_undergrounds, include_linked)
+  local fluidbox_targets = {}
+  for i = 1, #entity.fluidbox do
+    for _, pipe_connection in pairs(entity.fluidbox.get_pipe_connections(i)) do
+      if pipe_connection.target and (include_undergrounds and pipe_connection.connection_type ~= "linked" or include_linked and pipe_connection.connection_type ~= "underground" or pipe_connection.connection_type == "normal") then
+        fluidbox_targets[#fluidbox_targets+1] = {target = pipe_connection.target, target_fluidbox_index = pipe_connection.target_fluidbox_index, target_pipe_connection_index = pipe_connection.target_pipe_connection_index}
+      end
+    end
+  end
+  return fluidbox_targets
+end
+
+--- returns any fluidbox targets. by default only includes normal connections
+---@param entity LuaEntity 
+---@param include_undergrounds? boolean whether or not to include underground connections, default false
+---@param include_linked? boolean whether or not to include linked connections, default false
+---@return {target: LuaFluidBox, target_fluidbox_index: int, target_pipe_connection_index: int}[][] fluidbox_targets
+perel.get_fluidbox_targets_by_fluidbox_and_connection = function(entity, include_undergrounds, include_linked)
+  local fluidbox_targets = {}
+  for i = 1, #entity.fluidbox do
+    fluidbox_targets[i] = {}
+    for j, pipe_connection in pairs(entity.fluidbox.get_pipe_connections(i)) do
+      if pipe_connection.target and (include_undergrounds and pipe_connection.connection_type ~= "linked" or include_linked and pipe_connection.connection_type ~= "underground" or pipe_connection.connection_type == "normal") then
+        fluidbox_targets[i][j] = {target = pipe_connection.target, target_fluidbox_index = pipe_connection.target_fluidbox_index, target_pipe_connection_index = pipe_connection.target_pipe_connection_index}
+      end
+    end
+  end
+  return fluidbox_targets
+end
+
 --- returns the bitmask that represents the fluid connections of this entity. only includes connections that are valid and in use. may return nonsensical values when used on entities that do not have pipe-like connections
 ---@param entity LuaEntity 
 ---@param include_undergrounds? boolean whether or not to include underground connections, default false
+---@param include_linked? boolean whether or not to include linked connections, default false
 ---@return LuaEntity[] neighbours
-perel.get_pipe_connection_bitmask = function(entity, include_undergrounds)
+perel.get_pipe_connection_bitmask = function(entity, include_undergrounds, include_linked)
   local mask = 0
-  for _, neighbour in pairs(perel.get_fluidbox_neighoburs(entity, include_undergrounds)) do
+  for _, neighbour in pairs(perel.get_fluidbox_neighoburs(entity, include_undergrounds, include_linked)) do
     mask = mask + 2 ^ (perel.get_direction(entity.position, neighbour.position) / 4)
   end
   return mask
 end
 
 --- stores all of the normal connection categories found on an entity as a boolean dictionary. lazy loaded as required
----@type table<EntityID, table<string, boolean>>
+---@type {[string]: {[string]: true}}
 perel.entity_connection_categories = {}
 
 --- returns the connection category dictionary for an entity, and creates it if it don't already exist
 ---@param prototype LuaEntityPrototype
----@return table<string, boolean>
+---@return {[string]: true}
 perel.get_entity_connection_categories = function(prototype)
   if perel.entity_connection_categories[prototype.name] then return perel.entity_connection_categories[prototype.name] end
   perel.entity_connection_categories[prototype.name] = {}
@@ -53,7 +108,7 @@ end
 --- returns the neighbours that might connect to the entity's fluidboxes.
 --- may return duplicate entries if an entity collides with multiple pipe connection targets
 ---@param entity LuaEntity
----@param categories? table<string, boolean>
+---@param categories? {[string]: true}
 ---@return LuaEntity[] neighbours
 perel.get_possible_fluidbox_neighbours = function(entity, categories)
   local neighbours = {}
@@ -70,6 +125,37 @@ perel.get_possible_fluidbox_neighbours = function(entity, categories)
           for category in pairs(perel.get_entity_connection_categories(e.type == "entity-ghost" and e.ghost_prototype or e.prototype)) do
             if categories[category] then
               neighbours[#neighbours+1] = e
+              break
+            end
+          end
+        end
+      end
+    end
+  end
+  return neighbours
+end
+
+--- returns the neighbours that might connect to the entity's fluidboxes, indexed by fluidbox and connection id
+--- may return duplicate entries if an entity collides with multiple pipe connection targets
+---@param entity LuaEntity
+---@param categories? {[string]: true}
+---@return LuaEntity[][] neighbours
+perel.get_possible_fluidbox_neighbours_by_fluidbox_and_connection = function(entity, categories)
+  local neighbours = {}
+  categories = categories or perel.get_entity_connection_categories(entity.type == "entity-ghost" and entity.ghost_prototype or entity.prototype)
+  if not next(categories) then return {} end
+  for i = 1, #entity.fluidbox do
+    neighbours[i] = {}
+    for j, pipe_connection in pairs(entity.fluidbox.get_pipe_connections(i)) do
+      if pipe_connection.target then
+        neighbours[i][j] = pipe_connection.target.owner
+      elseif not pipe_connection.target then
+        for _, e in pairs(entity.surface.find_entities_filtered{
+          position = pipe_connection.target_position
+        }) do
+          for category in pairs(perel.get_entity_connection_categories(e.type == "entity-ghost" and e.ghost_prototype or e.prototype)) do
+            if categories[category] then
+              neighbours[i][j] = e
               break
             end
           end
