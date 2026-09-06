d073b5f27938a72bc0986341d91ccf8053c812ea
protocol_1903 <67478786+protocol-1903@users.noreply.github.com>
2026-09-05 19:11:27 -0700

    fix annotation

--- util/scripts/general.lua
+++ util/scripts/general.lua
@@ -7,7 +7,7 @@
 
 ---@class (partial) PEREL
 ---@field find_build_action fun(actions: UndoRedoAction[], entity: LuaEntity): uint32? Returns the undo action associated with this entity
----@field find_build_item fun(stack: LuaUndoRedoStack, entity: LuaEntity): [uint32?, uint32?] Returns the undo item and action associated with this entity
+---@field find_build_item fun(stack?: LuaUndoRedoStack, entity: LuaEntity): [uint32?, uint32?] Returns the undo item and action associated with this entity
 ---@field get_side_length fun(prototype: LuaEntityPrototype): double Returns the longest edge of the bounding box, or the length of a side for a square entity
 ---@field get_direction fun(a: MapPosition, b: MapPosition): defines.direction Returns the direction from a to b, assuming the two are in a straight line
 perel = perel or {}
@@ -32,7 +32,7 @@ perel.find_build_action = function(actions, entity)
 end
 
 ---Returns the undo item and action associated with this entity
----@param stack LuaUndoRedoStack
+---@param stack? LuaUndoRedoStack
 ---@param entity LuaEntity
 ---@return uint32? item_index, uint32? action_index
 perel.find_build_item = function(stack, entity)
